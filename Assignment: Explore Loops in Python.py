f0b0d389409cb56e36c050ce6a2195f0644c0bf3
Git-Amate <78541652+Git-Amate@users.noreply.github.com>
2025-05-14 17:26:14 -0400

    Add files via upload

---
+++ Assignment: Explore Loops in Python.py
@@ -0,0 +1,27 @@
+print("Task 1 - Counting Down with Loops")
+
+i = int(input("Enter the starting number:"))
+print(i)
+while i != 1:
+    i -= 1
+    print(i)
+print("Blast off! \n")
+
+print("Task 2 - Multiplication Table with for Loops")
+
+j = int(input("Enter the starting number:"))
+for i in range (0,11):
+    print(f"{j} x {i} == {i*j} \n")
+
+print("Task 3 - Find the Factorial")
+
+x = int(input("Enter the starting number:"))
+facto = x
+for y in range(x-1,0,-1):
+    print(facto)
+    print(y)
+    facto = facto * y
+
+print(f"The factorial of {x} is {facto}")
+
+
