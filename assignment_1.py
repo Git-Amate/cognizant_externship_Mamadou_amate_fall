44aff58295dfc4205bf8ab997bfb20fd01f5099e
Git-Amate <78541652+Git-Amate@users.noreply.github.com>
2025-05-14 16:45:17 -0400

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,27 @@
+print("Task 1 - Variables: Your First Python Intro")
+
+name = "Axel"
+age = 26
+gpa = 4.3
+
+print(f"hello my brother, i'm {name} it's been a long, i'm now {age} ! and i got {gpa} GPA in college \n")
+
+print("now let's do some math")
+num1 = 12
+num2 = 5
+print("the sum of 12 and 5 is", num1 + num2, "\n")
+
+print("Task 3 - Conditional Statements: The Number Checker ")
+num = int(input("please enter an number : "))
+
+print(f"Your number is {num}, now let's check if the number you put is positive, negative or zero")
+if num > 0:
+    print("This number is positive. Awesome!")
+elif num < 0:
+    print("This number is negative. Better luck next time!")
+else:
+    print("Zero it is. A perfect balance!")
+
+
+
+
